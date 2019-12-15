54c66ef774d6fab85686043411a65c84aff20b41
MattCallaway <40869356+MattCallawaySJSU@users.noreply.github.com>
2019-12-14 16:45:01 -0800

    Initial commit

---
+++ Debug/Third-Party/FreeRTOS/org/Source/tasks.d
@@ -0,0 +1,50 @@
+Third-Party/FreeRTOS/org/Source/tasks.o: \
+ ../Third-Party/FreeRTOS/org/Source/tasks.c \
+ D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/FreeRTOS/org/Source/include/FreeRTOS.h \
+ D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Config/FreeRTOSConfig.h \
+ D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h \
+ D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/SEGGER/SEGGER/SEGGER_SYSVIEW.h \
+ D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/SEGGER/SEGGER/SEGGER.h \
+ D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/SEGGER/Config/Global.h \
+ D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/FreeRTOS/org/Source/include/projdefs.h \
+ D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/FreeRTOS/org/Source/include/portable.h \
+ D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/FreeRTOS/org/Source/include/deprecated_definitions.h \
+ D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/FreeRTOS/org/Source/include/mpu_wrappers.h \
+ D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/FreeRTOS/org/Source/include/task.h \
+ D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/FreeRTOS/org/Source/include/list.h \
+ D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/FreeRTOS/org/Source/include/timers.h \
+ D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/FreeRTOS/org/Source/include/task.h \
+ D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/FreeRTOS/org/Source/include/stack_macros.h
+
+D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/FreeRTOS/org/Source/include/FreeRTOS.h:
+
+D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Config/FreeRTOSConfig.h:
+
+D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h:
+
+D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/SEGGER/SEGGER/SEGGER_SYSVIEW.h:
+
+D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/SEGGER/SEGGER/SEGGER.h:
+
+D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/SEGGER/Config/Global.h:
+
+D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/FreeRTOS/org/Source/include/projdefs.h:
+
+D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/FreeRTOS/org/Source/include/portable.h:
+
+D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/FreeRTOS/org/Source/include/deprecated_definitions.h:
+
+D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h:
+
+D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/FreeRTOS/org/Source/include/mpu_wrappers.h:
+
+D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/FreeRTOS/org/Source/include/task.h:
+
+D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/FreeRTOS/org/Source/include/list.h:
+
+D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/FreeRTOS/org/Source/include/timers.h:
+
+D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/FreeRTOS/org/Source/include/task.h:
+
+D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/FreeRTOS/org/Source/include/stack_macros.h:
---
+++ Debug/Third-Party/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.d
@@ -0,0 +1,44 @@
+Third-Party/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.o: \
+ ../Third-Party/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.c \
+ D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/FreeRTOS/org/Source/include/FreeRTOS.h \
+ D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Config/FreeRTOSConfig.h \
+ D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h \
+ D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/SEGGER/SEGGER/SEGGER_SYSVIEW.h \
+ D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/SEGGER/SEGGER/SEGGER.h \
+ D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/SEGGER/Config/Global.h \
+ D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/FreeRTOS/org/Source/include/projdefs.h \
+ D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/FreeRTOS/org/Source/include/portable.h \
+ D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/FreeRTOS/org/Source/include/deprecated_definitions.h \
+ D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/FreeRTOS/org/Source/include/mpu_wrappers.h \
+ D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/FreeRTOS/org/Source/include/task.h \
+ D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/FreeRTOS/org/Source/include/list.h \
+ ../Third-Party/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h
+
+D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/FreeRTOS/org/Source/include/FreeRTOS.h:
+
+D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Config/FreeRTOSConfig.h:
+
+D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h:
+
+D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/SEGGER/SEGGER/SEGGER_SYSVIEW.h:
+
+D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/SEGGER/SEGGER/SEGGER.h:
+
+D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/SEGGER/Config/Global.h:
+
+D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/FreeRTOS/org/Source/include/projdefs.h:
+
+D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/FreeRTOS/org/Source/include/portable.h:
+
+D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/FreeRTOS/org/Source/include/deprecated_definitions.h:
+
+D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h:
+
+D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/FreeRTOS/org/Source/include/mpu_wrappers.h:
+
+D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/FreeRTOS/org/Source/include/task.h:
+
+D:/Users/Matt/workspace/STM32_FreeRTOS_Tasks_Priority/Third-Party/FreeRTOS/org/Source/include/list.h:
+
+../Third-Party/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h:
